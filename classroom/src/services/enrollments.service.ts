f8a77bf3d0a5015f2c1f5dce191e71356b19c414
Raphael Berger <raphaelmb@gmail.com>
2022-04-11 16:27:46 -0300

    Resolvers and Queries

--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 
+interface GetByCourseAndStudentId {
+  courseId: string;
+  studentId: string;
+}
+
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
+
+  getByCourseAndStudentId({ courseId, studentId }: GetByCourseAndStudentId) {
+    return this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
+        studentId,
+        canceledAt: null,
+      },
+    });
+  }
 
   listAllEnrollments() {
     return this.prisma.enrollment.findMany({
