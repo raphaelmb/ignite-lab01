5c0633e919e32b07167604016c6b6610b386cff2
Raphael Berger <raphaelmb@gmail.com>
2022-04-14 15:30:55 -0300

    Started frontend integration

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,15 +1,21 @@
+import { gql, useQuery } from "@apollo/client";
 import {
   getAccessToken,
   useUser,
   withPageAuthRequired,
 } from "@auth0/nextjs-auth0";
+import { withApollo } from "../../lib/withApollo";
 
-export default function Home() {
+const PRODUCTS_QUERY = gql``;
+
+function Home() {
   const { user } = useUser();
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
 
   return (
     <div>
       <h1>Hello World</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
@@ -17,10 +23,12 @@ export default function Home() {
 
 export const getServerSideProps = withPageAuthRequired({
   getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res));
+    // console.log(getAccessToken(req, res));
 
     return {
       props: {},
     };
   },
 });
+
+export default withApollo(Home);
